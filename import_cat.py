8d02b129c7a379f615e2ec767112f0e36b603250
kaakdela <gunjubasich@gmail.com>
2021-03-10 23:04:51 +0500

    Delete import_cat.py

--- import_cat.py
+++
@@ -1,5 +0,0 @@
-from cat import Cat
-c1 = Cat("Барон", "мальчик", 2)
-c2 = Cat("Сэм", "мальчик", 2)
-print(c1.get_name(), c1.get_sex(), c1.get_age())
-print(c2.get_name(), c2.get_sex(), c2.get_age())
